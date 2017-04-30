283e383202a1007821e53f124583fbfd624a7dfb
Warren Edgar <wew6@wildcats.unh.edu>
2017-04-30 16:59:25 -0400

    added MPI shell info

--- brooks_iyengar.c
+++ brooks_iyengar.c
@@ -1,16 +1,14 @@
+/* see brooks_iyengar.h for includes */
 #include "brooks_iyengar.h"
-
-
 
 int main( int argc, char ** argv ){
 
 
+  int info [MPI_INFO]; 
+  MPI_SETUP(argc, argv, info[i_RANK], info[i_SIZE]);
+  DBG("Hello I am #%d out of %d.\n", info[i_RANK], info[i_SIZE]);
 
 
-
-
-
-
-
+  MPI_FINISH();
   return 0;
 }
--- brooks_iyengar.h
+++ brooks_iyengar.h
@@ -1,8 +1,28 @@
 #ifndef BROOKSIYENGAR_H
 #define BROOKSIYENGAR_H
 
+#include <mpi.h>
+#include <stdio.h>
 
+/* index of MPI data in info array*/
+#define i_RANK 0x0
+#define i_SIZE 0x1
+#define MPI_INFO 0x2
 
+/* set up any MPI related info */
+#define MPI_SETUP(argc, argv, rank, size)\
+  MPI_Init(&argc, &argv);\
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);\
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+/* clean up any MPI related info */
+#define MPI_FINISH()\
+  MPI_Finalize();
+
+/* DEBUG DEBUG DEBUG */
+#define DEBUG 1
+
+#define DBG(fmt, ...) \
+    if (DEBUG) fprintf(stderr, fmt, __VA_ARGS__);
 
 #endif /* BROOKSIYENGAR_H */
